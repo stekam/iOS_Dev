92ae340da762a59e783bf3bd8a55b6ff47fa108e
Stephan <phonegap@stekam.com>
2012-11-01 15:43:32 +0100

    DayLight Initial Version
    DayLight Initial Version

---
+++ DayLight/DayLight/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  DayLight
+//
+//  Created by Stephan Kaminski on 01/11/12.
+//  Copyright (c) 2012 SK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        
+        // insert code here...
+        NSLog(@"Hello, World Baboum!");
+        
+    }
+    return 0;
+}
+
